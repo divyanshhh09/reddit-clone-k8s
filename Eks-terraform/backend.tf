44fdb79bf46629345c70b73ffae7f715b1ba90ce
Divyansh Parmar <138980298+divyanshhh09@users.noreply.github.com>
2024-09-19 18:48:13 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "dino1234509" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "eu-east-1"
   }
 }
